a0e454779931cb615013ddad3b788a4909bdf207
Kassin Dornelles <40970965+kassindornelles@users.noreply.github.com>
2018-10-24 20:51:52 -0300

    change fakeso value to false by default

--- Teste/My Project/Settings.Designer.vb
+++ Teste/My Project/Settings.Designer.vb
@@ -65,7 +65,7 @@ Namespace My
         
         <Global.System.Configuration.UserScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("False")>  _
         Public Property fakeso() As String
             Get
                 Return CType(Me("fakeso"),String)
